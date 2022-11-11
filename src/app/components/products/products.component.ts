6f5bf4747e4e99acb212b8dff5cf63b6326e44c5
SgtGarro <marcelogarro137@gmail.com>
2022-11-11 12:41:47 -0500

    Knowing the pipes

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -12,8 +12,9 @@ export class ProductsComponent implements OnInit {
 
   myShoppingCart: Product[] = [ ];
   total: number = 0;
-
   products: Product[] = [];
+  today = new Date();
+  date = new Date(2021, 1, 21);
 
   constructor(private storeService: StoreService, private productsService: ProductsService) {
     this.myShoppingCart = this.storeService.getShoppingCart();
