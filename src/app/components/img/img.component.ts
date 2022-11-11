db9e97a39cf48b3274b684d4a29ad531f0d4737b
SgtGarro <marcelogarro137@gmail.com>
2022-11-10 23:11:20 -0500

    Components and Header

--- src/app/components/img/img.component.ts
+++ src/app/components/img/img.component.ts
@@ -1,19 +1,22 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-img',
   templateUrl: './img.component.html',
   styleUrls: ['./img.component.scss']
 })
-export class ImgComponent implements OnInit {
+export class ImgComponent implements OnInit, OnDestroy {
   @Input() img: string = '';
   @Output() loaded = new EventEmitter<string>();
   imgDefault: string = 'https://thumbs.dreamstime.com/z/icono-del-perfil-avatar-defecto-105356015.jpg';
+  counter: number = 0;
+  counterFn: number | undefined;
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  ngOnDestroy(): void {
+  }
   imgError() {
     this.img = this.imgDefault;
   }
