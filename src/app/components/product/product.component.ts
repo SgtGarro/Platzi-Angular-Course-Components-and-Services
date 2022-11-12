504c8b0ed48a56d756873e688060483b791c93eb
SgtGarro <marcelogarro137@gmail.com>
2022-11-12 09:32:29 -0500

    Fixes

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -5,14 +5,11 @@ import { Product } from 'src/app/models/product.model';
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.scss']
 })
-export class ProductComponent implements OnInit {
+export class ProductComponent {
 
   @Input() product!: Product;
   @Output() addedProduct = new EventEmitter<Product>();
   constructor() { }
-  ngOnInit(): void {
-    throw new Error('Method not implemented.');
-  }
 
   onAddToCart() {
     this.addedProduct.emit(this.product);
